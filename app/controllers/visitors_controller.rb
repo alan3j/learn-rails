6861038890b2daf4d32ba2e0ebe0486923001477
Craig J <craig.jantzen@gmail.com>
2016-02-27 22:00:37 -0600

    Views, layouts, etc. 1

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,13 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
+		
+		# default
+		render 'visitors/new'
+		
+		# no layout ==> skip /app/views/layouts/application.html.erb
+		#-- render 'visitors/new', :layout => false
+
 	end
 	
 end
