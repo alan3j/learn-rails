12236f0a65a8ecdb92fad8a71b4ad5dd1dbee619
Craig J <craig.jantzen@gmail.com>
2016-02-28 10:01:06 -0600

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -9,9 +9,9 @@ class VisitorsController < ApplicationController
 		# no layout ==> skip /app/views/layouts/application.html.erb
 		#-- render 'visitors/new', :layout => false
 
-		flash[:notice] = 'A flash notice'
-		flash[:alert]  = 'A flash alert'
-		flash.now[:alert] = 'A flash.now alert'
+		#-- flash[:notice] = 'A flash notice'
+		#-- flash[:alert]  = 'A flash alert'
+		#-- flash.now[:alert] = 'A flash.now alert'
 
 	end
 	
